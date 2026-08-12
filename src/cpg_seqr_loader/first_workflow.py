3698eac19a21efaf15e6b21c0b32a97bd5f3e225
EddieLF <eddieformaini@gmail.com>
2026-08-12 13:06:23 +1000

    Add DeleteCombinerTemp back in as terminal stage

--- src/cpg_seqr_loader/first_workflow.py
+++ src/cpg_seqr_loader/first_workflow.py
@@ -8,7 +8,7 @@ import argparse
 
 from cpg_flow import workflow
 
-from cpg_seqr_loader.stages import CreateDenseMtFromVdsWithHail, SubmitPostCombinerWorkflow
+from cpg_seqr_loader.stages import CreateDenseMtFromVdsWithHail, DeleteCombinerTemp, SubmitPostCombinerWorkflow
 
 
 def cli_main():
@@ -18,7 +18,7 @@ def cli_main():
 
     workflow.run_workflow(
         name='seqr_loader',
-        stages=[CreateDenseMtFromVdsWithHail, SubmitPostCombinerWorkflow],
+        stages=[CreateDenseMtFromVdsWithHail, DeleteCombinerTemp, SubmitPostCombinerWorkflow],
         dry_run=args.dry_run,
     )
 
